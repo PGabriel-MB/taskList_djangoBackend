c23b9d8bcd28635bf6356dc4517e10a05ef0847d
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-12-14 21:52:28 -0300

    corrigindo alguns itens de backend

--- lgpd/urls.py
+++ lgpd/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_auth import urls
 from apps.tasks.urls import router
 
 urlpatterns = [
