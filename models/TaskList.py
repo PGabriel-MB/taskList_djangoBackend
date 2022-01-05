1ee026e270610e2e708f09a54235aaa9d96dff26
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-04 21:50:16 -0300

    ajustados models de tarafa e lista de tarefa

--- models/TaskList.py
+++ models/TaskList.py
@@ -3,7 +3,8 @@ from .Base import BaseModel
 from .User import User
 
 class TaskList(BaseModel):
-    user = db.ReferenceField(User, reverse_delete_rule=db.CASCADE)
-
     name = db.StringField(required=True)
     dead_line = db.DateTimeField(required=False)
+    tasks = db.ListField(
+        db.ReferenceField('Task', reverse_delete_rule=db.CASCADE)
+    )
--- models/User.py
+++ models/User.py
@@ -6,4 +6,8 @@ from .Base import BaseModel
 class User(BaseModel):
     name = db.StringField(required=True, unique=True)
     email = db.EmailField(requred=True)
-    password = db.StringField(required=True)
+    password = db.StringField(required=True, min_length=6)
+
+    task_lists = db.ListField(
+        db.ReferenceField('TaskList', revese_delete_rule=db.CASCADE)
+    )
