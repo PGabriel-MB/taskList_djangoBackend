69a4d77fcbbfae0fadd407f17a2b3abbc4d0f59d
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-25 18:09:39 -0300

    trabalhando nos erros de autenticaçao

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask_jwt_extended import JWTManager
 
 from database.db import initialize_db
 from routes import initialize_routes
-from models.User import User
+from resources.errors import errors
 
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
 
-api = Api(app)
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
--- resources/task.py
+++ resources/task.py
@@ -1,8 +1,12 @@
 from flask import Response, request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
+from mongoengine.errors import FieldDoesNotExist, \
+    NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
 
 from models.Task import Task
+from resources.errors import SchemaValidationError, TaskAlreadyExistsError, \
+    InternalServerError, UpdatingTaskError, DeletingTaskError, TaskNotExistsError
 
 
 class TasksApi(Resource):
@@ -13,25 +17,49 @@ class TasksApi(Resource):
 
     @jwt_required
     def post(self):
-        body = request.get_json()
-        task = Task(**body).save()
-        id = task.id
-        return { 'id': str(id) }, 200
+        try:
+            body = request.get_json()
+            task = Task(**body).save()
+            id = task.id
+            return { 'id': str(id) }, 200
+        except (FieldDoesNotExist, ValidationError):
+            raise SchemaValidationError
+        except NotUniqueError:
+            raise TaskAlreadyExistsError
+        except Exception as e:
+            raise InternalServerError
 
 
 class TaskApi(Resource):
     @jwt_required
     def put(self, id):
-        body = request.get_json()
-        Task.objects.get(id=id).update(**body)
-        return '', 200
+        try:    
+            body = request.get_json()
+            Task.objects.get(id=id).update(**body)
+            return '', 200
+        except InvalidQueryError:
+            raise SchemaValidationError
+        except DoesNotExist:
+            raise UpdatingTaskError
+        except Exception:
+            raise InternalServerError
     
     @jwt_required
     def delete(self, id):
-        task = Task.objects.get(id=id).delete()
-        return '', 200
+        try:
+            task = Task.objects.get(id=id).delete()
+            return '', 200
+        except DoesNotExist:
+            raise DeletingTaskError
+        except Exception:
+            raise InternalServerError
     
     @jwt_required
     def get(self, id):
-        task = Task.objects.get(id=id).to_json()
-        return Response(task, mimetype="application/json", status=200)
+        try:
+            task = Task.objects.get(id=id).to_json()
+            return Response(task, mimetype="application/json", status=200)
+        except DoesNotExist:
+            raise TaskNotExistsError
+        except Exception:
+            raise InternalServerError
